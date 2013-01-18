85acb8a24cfa1c26cb1d63647da61bc36c10e633
sushmitharaos <sushmitharaos@gmail.com>
2013-01-18 17:54:04 +0530

    Vinay,Sush | #676 | added validation logic to number field

--- src/outpatient/app/view/concept/NumericUIElement.js
+++ src/outpatient/app/view/concept/NumericUIElement.js
@@ -2,45 +2,66 @@ Ext.define('Jss.Outpatient.view.concept.NumericUIElement', {
     extend:'Ext.form.Panel',
 
     config:{
-        height:50,
+        height:100,
         flex:1,
         scrollable:'false',
-        layout:'fit',
+        layout:'hbox',
     },
 
-    for: function(concept) {
+    for:function (concept) {
         this.concept = concept;
-        this.numberfield = Ext.create('Ext.field.Number',{
-            height: 50,
-            label: concept.name,
-            name:"value",
+        this.numberfield = Ext.create('Ext.field.Number', {
+            height:50,
+            width:'150%',
+            label:concept.name,
+            name:"value"
         });
-
+        
         this.add({
             xtype:'fieldset',
+            flex:1,
             items: [this.numberfield]
         })
 
+        this.add({
+            xtype:'label',
+            flex:1,
+            id:'errorMessage',
+            height:50,
+        })
         return this;
     },
 
-    getValue: function() {
+    getValue:function () {
         return Ext.create('Jss.Outpatient.model.history.Observation', {
-            concept: this.concept,
-            properties: this.numberfield.getValue(),
-            summary: this.getSummary(),
+            concept:this.concept,
+            properties:this.numberfield.getValue(),
+            summary:this.getSummary(),
         });
     },
 
-    getSummary: function() {
+    getSummary:function () {
         return this.concept.name + ":" + this.numberfield.getValue();
     },
 
-    isValid:function(){
-        return false;
+    isValid:function () {
+        var value = this.numberfield.getValue();
+        if (value == null || value == undefined) return false;
+
+        var properties = this.concept.properties.datatype.properties;
+
+        if (properties == null || properties == undefined) return true;
+
+        var result = true;
+        if (properties.hiAbsolute) result = (result && properties.hiAbsolute >= value);
+        if (properties.lowAbsolute) result = (result && properties.lowAbsolute <= value);
+        console.log(Ext.getCmp("errorMessage"));
+        Ext.getCmp("errorMessage").setHtml('Value not in allowed range');
+
+        return result;
     },
 
-    highlightErrors:function(){
+    highlightErrors:function () {
         this.numberfield.setStyle('background:red');
     }
 });
