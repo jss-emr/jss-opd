3d148d3e5d85e96b279b514c3c8442811f06fc8d
sushmitharaos <sushmitharaos@gmail.com>
2013-02-01 14:23:20 +0530

    Sush|#684 | added modal window for edit screen to allow edit/ delete of item

--- src/outpatient/app/view/instruction/InstructionEdit.js
+++ src/outpatient/app/view/instruction/InstructionEdit.js
@@ -1,38 +1,75 @@
 Ext.define('Jss.Outpatient.view.instruction.InstructionEdit', {
-    extend: 'Ext.Container',
-    alias: 'widget.instruction-edit',
+    extend:'Ext.Container',
+    alias:'widget.instruction-edit',
 
-    config: {
-        layout: 'vbox',
-        items: [{
-            xtype: 'list',
-            id: 'instruction-observationSummaryPanel',
-            itemTpl: '{summary}',
-            store: 'Instructions',
-            cls: 'diagnosisSummary stripedList',
-            flex: 1,
-            inline: true,
-            scrollable: 'vertical',
-            listeners: {
-                itemswipe: function(list, index, target, record, event) {
-                    if(event.direction == 'right')
-                        record.set('discarded', true);
-                    else
-                        record.set('discarded', false);
+    config:{
+        layout:'vbox',
+        items:[
+            {
+                xtype:'list',
+                id:'instruction-observationSummaryPanel',
+                itemTpl:'{summary}',
+                store:'Instructions',
+                cls:'diagnosisSummary stripedList',
+                flex:1,
+                inline:true,
+                scrollable:'vertical',
+                listeners:{
+                    itemtaphold:function (list, index, target, record) {
+                        Ext.create('Ext.Panel', {
+                            id:'optionsPanel',
+                            left:0,
+                            padding:10,
+                            hideOnMaskTap:true,
+                            modal:true,
+                            showAnimation:'fadeIn',
+                            layout:{
+                                type:'hbox',
+                                align:'middle'
+                            },
+                            items:[
+                                {
+                                    xtype:'button',
+                                    id:'listItemEdit',
+                                    iconCls:'compose',
+                                    iconMask:true,
+                                    padding:10,
+                                    listeners:{
+                                        tap:function(){
+                                            console.log("edit button tap");
+                                        }
+                                    }
+                                },
+                                {
+                                    xtype:'button',
+                                    id:'listItemDelete',
+                                    iconCls:'trash',
+                                    iconMask:true,
+                                    padding:10,
+                                    listeners: {
+                                        tap:function(){
+                                            Ext.getCmp('instruction-observationSummaryPanel').getStore().remove(record);
+                                            Ext.getCmp('optionsPanel').hide(true);
+                                        }
+                                    }
+                                }
+                            ]
+                        }).showBy(target);
+                    }
                 }
+            },
+            {
+                xtype:'addObservationPanel',
+                id:'instruction-addObservationsPanel',
+                scrollable:false,
+                autoCompleteStore:'AllInstructions',
+                observationQueueStore:'InstructionsQueue',
+                autoCompleteItemTpl:'{name}',
+                autoCompleteFilterKey:'name',
+                flex:3,
             }
-        },
-        {
-            xtype : 'addObservationPanel',
-            id:'instruction-addObservationsPanel',
-            scrollable: false,
-            autoCompleteStore: 'AllInstructions',
-            observationQueueStore: 'InstructionsQueue',
-            autoCompleteItemTpl: '{name}',
-            autoCompleteFilterKey: 'name',
-            flex: 3,
-        }
         ]
+    },
 
-    }
+
 });
